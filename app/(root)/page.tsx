c108b9d9ad0c7f63e24ed46e5fac17e6ff834502
shahab-ramzan <shahab.ramzan@codedistrict.com>
2023-07-18 20:25:22 +0500

    modal added with zustannd

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+  const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
+};
+
+export default SetupPage;
